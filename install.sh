244eb5ab321e3cc7f88f553ab55940212d48ebea
di-ov <dmarkov42@protonmail.com>
2020-03-29 10:41:03 +0200

    Riced my i3status config.

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 DOTFILES_DIR=~/dogfiles
 
 # Create symlics and remove existing destination files
-ln -sfv $DOTFILES_DIR/i3wm/config ~/.config/i3/config	
+ln -sfv $DOTFILES_DIR/i3wm/config ~/.config/i3/config
+mkdir -p ~/.config/i3status && ln -sfv $DOTFILES_DIR/bar/i3status/config ~/.config/i3status/config
+
+
+
 # ln -sfv $DOTFILES_DIR/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
