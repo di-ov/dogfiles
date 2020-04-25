b55bb5a291f4dbe9971c4353ce5bad85c6d4741e
di-ov <dmarkov42@protonmail.com>
2020-04-25 18:17:54 +0200

    Added wallpaper.

--- bootstrap/install-looks.sh
+++ bootstrap/install-looks.sh
@@ -4,6 +4,8 @@ ln -sfv $DOTFILES_DIR/looks/.Xresources ~/.Xresources
 ln -sfv $DOTFILES_DIR/looks/gtk2.0/.gtkrc-2.0 ~/.gtkrc-2.0
 ln -sfv $DOTFILES_DIR/looks/gtk-3.0/.settings.ini ~/.config/gtk-3.0/.settings.ini
 
+lv -sfv $DOTFILES_DIR/looks/.wallpaper.jpg ~/.wallpaper.jpg
+
 mkdir -pv ~/.fonts && cp -rfv $DOTFILES_DIR/looks/fonts/* ~/.fonts/
 mkdir -pv ~/.themes && cp -rfv $DOTFILES_DIR/looks/themes/* ~/.themes/
 mkdir -pv ~/.icons && cp -rfv $DOTFILES_DIR/looks/icons/* ~/.icons/
