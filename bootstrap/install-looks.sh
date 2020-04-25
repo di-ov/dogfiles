655c8a96913c51fa674114fc687df2a4f7c020c6
di-ov <dmarkov42@protonmail.com>
2020-04-25 18:22:24 +0200

    Fixed a typo.

--- bootstrap/install-looks.sh
+++ bootstrap/install-looks.sh
@@ -4,7 +4,7 @@ ln -sfv $DOTFILES_DIR/looks/.Xresources ~/.Xresources
 ln -sfv $DOTFILES_DIR/looks/gtk2.0/.gtkrc-2.0 ~/.gtkrc-2.0
 ln -sfv $DOTFILES_DIR/looks/gtk-3.0/.settings.ini ~/.config/gtk-3.0/.settings.ini
 
-lv -sfv $DOTFILES_DIR/looks/.wallpaper.jpg ~/.wallpaper.jpg
+ln -sfv $DOTFILES_DIR/looks/.wallpaper.jpg ~/.wallpaper.jpg
 
 mkdir -pv ~/.fonts && cp -rfv $DOTFILES_DIR/looks/fonts/* ~/.fonts/
 mkdir -pv ~/.themes && cp -rfv $DOTFILES_DIR/looks/themes/* ~/.themes/
