c9ddbaf377b7831ce0238456b1877352302c65e9
di-ov <dmarkov42@protonmail.com>
2020-05-05 16:47:04 +0200

    Made scipt for linking vscode dotfiles.

---
+++ bootstrap/setup-vscode.sh
@@ -0,0 +1,4 @@
+DOTFILES_DIR=~/dogfiles
+
+ln -sfv $DOTFILES_DIR/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -sfv $DOTFILES_DIR/vscode/settings.json ~/.config/Code/User/settings.json
