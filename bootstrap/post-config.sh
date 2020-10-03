dba4420c02c40d678ffcb461a4a3b643bbf671dc
di-ov <dmarkov42@protonmail.com>
2020-10-03 11:57:41 +0200

    Updates to terminal setup.

--- bootstrap/post-config.sh
+++ bootstrap/post-config.sh
@@ -3,7 +3,7 @@ chsh -s /usr/bin/zsh
 cd /etc/zsh && rm -f newuser.zshrc.recommended  zlogin  zlogout  zprofile  zshenv  zshrc
 
 cp /home/di-ov/dogfiles/bootstrap/packages/exa /usr/local/bin
-dpkg -i /home/di-ov/dogfiles/bootstrap/packages/Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
+cp /home/di-ov/dogfiles/bootstrap/packages/alacritty /usr/local/bin
 
 # Removes pager in "git diff" command
 git config --global pager.diff false
