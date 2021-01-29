29c536db0414a2ac281478e16e0e7672f91cc9ad
di-ov <dmarkov42@protonmail.com>
2021-01-29 21:47:46 +0200

    Archived themes.

--- install/install-looks.sh
+++ install/install-looks.sh
@@ -1,14 +1,21 @@
-DOTFILES_DIR=~/dogfiles
+LOOKS_DIR=~/dogfiles/looks
 
-ln -sfv $DOTFILES_DIR/looks/.Xresources ~/.Xresources
-ln -sfv $DOTFILES_DIR/looks/gtk2.0/.gtkrc-2.0 ~/.gtkrc-2.0
-ln -sfv $DOTFILES_DIR/looks/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+ln -sfv $LOOKS_DIR/.Xresources ~/.Xresources
+ln -sfv $LOOKS_DIR/gtk2.0/.gtkrc-2.0 ~/.gtkrc-2.0
+ln -sfv $LOOKS_DIR/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
 
-ln -sfv $DOTFILES_DIR/looks/.wallpaper.jpg ~/.wallpaper.jpg
+ln -sfv $LOOKS_DIR/.wallpaper.jpg ~/.wallpaper.jpg
 
-mkdir -pv ~/.fonts && cp -rfv $DOTFILES_DIR/looks/fonts/* ~/.fonts/
-mkdir -pv ~/.themes && cp -rfv $DOTFILES_DIR/looks/themes/* ~/.themes/
-mkdir -pv ~/.icons && cp -rfv $DOTFILES_DIR/looks/icons/* ~/.icons/
+mkdir -pv ~/.fonts && cp -rfv $LOOKS_DIR/fonts/* ~/.fonts/
+mkdir -pv ~/.themes && cp -rfv $LOOKS_DIR/themes/* ~/.themes/
+mkdir -pv ~/.icons && cp -rfv $LOOKS_DIR/icons/* ~/.icons/
 
+cd ~/.fonts && tar -xzvf fonts.tar.gz
+cd ~/.themes && tar -xzvf themes.tar.gz
+cd ~/.icons && tar -xzvf icons.tar.gz
+
+rm ~/.fonts/fonts.tar.gz
+rm ~/.themes/themes.tar.gz
+rm ~/.icons/icons.tar.gz
 # Reset font cache
 fc-cache -fv
